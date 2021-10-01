173181e6f8cc3572d892df1d8458a7ea5e7e3824
marcos-bonfanti <ext_mabonfan@mercadolibre.com>
2021-09-30 22:11:29 -0300

    Cookies session

--- src/services/server.ts
+++ src/services/server.ts
@@ -5,6 +5,13 @@ import { initWSServer } from './socket'
 import apiRouter from '../routes/index';
 import handlebars from 'express-handlebars';
 import vistaRouter from '../routes/vista';
+import session from 'express-session';
+
+
+const myUser = 'pepe';
+const myPassword = 'BokitaTheBiggest';
+const oneDay = 1000 * 60;
+
 
 const app = express();
 
@@ -29,12 +36,37 @@ app.engine(
 );
 
 app.use(express.json());
+app.use(
+  session({
+    secret: 'thisismysecrctekeyfhrgfgrfrty84fwir767',
+    cookie: { maxAge: 5000 },
+    saveUninitialized: true,
+    resave: false,
+  })
+);
 app.use('/api', apiRouter);
 app.use('/productos', vistaRouter);
 
 app.get('/', (req, res) => {
     const datos = []
     res.render('main', datos);
+});
+
+app.get('/login', (req, res) => {
+  const { username, password } = req.query;
+
+  if (username == myUser && password == myPassword) {
+    req.session['loggedIn'] = true;
+    req.session['contador'] = 1;
+    req.session['admin'] = true;
+    console.log(req.session);
+    res.render('bienvenido')
+  } else res.status(401).json({ msg: 'no estas autorizado' });
+});
+
+app.get('/logout', (req, res) => {
+  req.session.destroy;
+  res.redirect('/');
 });
 
 const myServer = http.createServer(app);
